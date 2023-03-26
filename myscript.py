889ab8d2bf2ebede9a6a6f8274ceec2a0a784e7f
michaelroussel404@outlook.com <michaelroussel404@outlook.com>
2023-03-25 22:57:30 -0400

    compatiblité python3

--- myscript.py
+++ myscript.py
@@ -1,5 +1,5 @@
 import os
 
 os.system("git bisect start c1a4be0 e4cfc6f")
-os.system("git bisect run python manage.py test")
+os.system("git bisect run python3 manage.py test")
 os.system("git bisect reset")
