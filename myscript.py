13010c42c6074086b37c3b2a4428dfbfeacab935
michaelroussel404@outlook.com <michaelroussel404@outlook.com>
2023-03-25 21:53:24 -0400

    Update myscript.py

--- myscript.py
+++ myscript.py
@@ -1,6 +1,6 @@
 import os
 
-os.system('git bisect start e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c c1a4be04b972b6c17db242fc37752ad517c29402')
+os.system('git bisect start c1a4be04b972b6c17db242fc37752ad517c29402 e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c')
 os.system('git bisect run python3 manage.py test')
 os.system('git bisect reset')
 
