94a8915edfcf94f1a7245fde06403e2047d30f60
michaelroussel404@outlook.com <michaelroussel404@outlook.com>
2023-03-25 21:43:22 -0400

    Modification du pipeline pour utiliser git bisect

---
+++ myscript.py
@@ -0,0 +1,9 @@
+import os
+
+goodhash = 'e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c'
+badhash = 'c1a4be04b972b6c17db242fc37752ad517c29402'
+
+os.system(f'git bisect start {badhash} {goodhash}')
+os.system('git bisect run python3 manage.py test')
+os.system('git bisect reset')
+
