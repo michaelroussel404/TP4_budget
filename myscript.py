b03ededc71f658ffc093a08590187f1c38f84283
michaelroussel404@outlook.com <michaelroussel404@outlook.com>
2023-03-25 22:35:35 -0400

    ...........

--- myscript.py
+++ myscript.py
@@ -1,7 +1,7 @@
 import os
 
 os.system("git bisect start")
-os.system("git bisect good e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c")
-os.system("git bisect bad c1a4be04b972b6c17db242fc37752ad517c29402")
+os.system("git bisect good e4cfc6f")
+os.system("git bisect bad c1a4be0")
 os.system("git bisect run python manage.py test")
 os.system("git bisect reset")
